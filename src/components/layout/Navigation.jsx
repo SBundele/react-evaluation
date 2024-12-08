c71f8b9fe800ba4f5760916bd30fe2dc38f68f47
Sourabh Bundele <sbundele0318@gmail.com>
2024-12-08 16:06:26 +0530

    added Home page where all countries are listed and also is responsive

---
+++ src/components/layout/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <nav className='navbar'>
+      <h1>Countries</h1>
+    </nav>
+  )
+}
+
+export default Navigation
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Home() {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCountries = async () => {
+    setLoading(true);
+    try {
+      let response = await fetch("https://restcountries.com/v3.1/all");
+      if (response.ok) {
+        let res = await response.json();
+        console.log(res);
+        setCountries(res);
+        setLoading(false);
+      }
+    } catch (e) {
+      console.log(error);
+      setLoading(false);
+      setCountries(["Some Error Occurred"]);
+    }
+  };
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  return (
+    <section className="country-section">
+      <div className="country-container">
+        {loading && <h1>Loading...</h1>}
+        {countries.map((country) => {
+          return (
+            <div className="card" key={country.name.common}>
+              <div className="card-img">
+                <img src={country.flags.png} alt="country-flag" />
+              </div>
+              <div className="card-body">
+                <h2>{country.name.common}</h2>
+                <ul>
+                  <li>
+                    <strong>Population:</strong> {country.population}
+                  </li>
+                  <li>
+                    <strong>Region:</strong> {country.region}
+                  </li>
+                  <li>
+                    <strong>Capital:</strong> {country.capital}
+                  </li>
+                </ul>
+              </div>
+              <div className="card-footer">
+                <button>More Details</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default Home;
